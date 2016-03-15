4d9aa603bb87dd8ed9915c182b7f5528d6a52d4c
Bogdan Timchenko <bogdantimchenko@yandex.ru>
2016-03-15 13:24:56 +0300

    Fixed handling errors

--- cat/cat.c
+++ cat/cat.c
@@ -4,6 +4,17 @@
 #include <unistd.h>		//rdwr
 #define BUF_SIZE 1023
 
+int reopen(int fd, int argc, char **argv) {
+	close(fd);
+	int nfd;
+	if (argc < 2) {
+		nfd = STDIN_FILENO;
+	} else {
+		nfd = open(argv[1], O_RDONLY);
+	}
+	return nfd;
+}
+
 int main(int argc, char **argv) {
 	int fd;
 	if (argc < 2) {
@@ -18,15 +29,27 @@ int main(int argc, char **argv) {
 	int rd_len = 0, wr_len = 0;
 	while (rd_len = read(fd, buf, BUF_SIZE)) {
 		if (rd_len == -1) {
-			write(STDERR_FILENO, "READ ERROR\n", 12);
-			continue;
+			fd = reopen(fd, argc, argv);
+			if (fd == -1) {
+				write(STDERR_FILENO, "FATAL READ ERROR\n", 18);
+				break;
+			} else {
+				write(STDERR_FILENO, "READ ERROR\n", 12);
+				continue;
+			}
 		}
 		int cmpl_len = 0;
 		while (rd_len - cmpl_len > 0) {
 			wr_len = write(STDOUT_FILENO, buf + cmpl_len, rd_len - cmpl_len);
 			if (wr_len == -1) {
-				write(STDERR_FILENO, "WRITE ERROR\n", 13);
-				continue;
+				fd = reopen(fd, argc, argv);
+				if (fd == -1) {
+					write(STDERR_FILENO, "FATAL WRITE ERROR\n", 19);
+					break;
+				} else {
+					write(STDERR_FILENO, "WRITE ERROR\n", 13);
+					continue;
+				}
 			}
 			cmpl_len += wr_len;
 		}
