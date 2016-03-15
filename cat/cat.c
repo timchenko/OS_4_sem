b2f9e5f0bc52d0b05eadc5ac2c926ab8a1620f59
Bogdan Timchenko <bogdantimchenko@yandex.ru>
2016-03-15 13:04:46 +0300

    Added work with file

--- cat/cat.c
+++ cat/cat.c
@@ -1,20 +1,32 @@
+#include <sys/types.h>	//open
+#include <sys/stat.h>   //open
+#include <fcntl.h>      //open
 #include <unistd.h>		//rdwr
 #define BUF_SIZE 1023
 
-int main() {
+int main(int argc, char **argv) {
+	int fd;
+	if (argc < 2) {
+		fd = STDIN_FILENO;
+	} else {
+		fd = open(argv[1], O_RDONLY);
+	}
+	if (fd == -1) {
+		write(STDERR_FILENO, "OPEN ERROR\n", 12);
+	}
 	char buf[BUF_SIZE];
 	int rd_len = 0, wr_len = 0;
-	while (rd_len = read(STDIN_FILENO, buf, BUF_SIZE)) {
+	while (rd_len = read(fd, buf, BUF_SIZE)) {
 		if (rd_len == -1) {
 			write(STDERR_FILENO, "READ ERROR\n", 12);
-			break;
+			continue;
 		}
 		int cmpl_len = 0;
 		while (rd_len - cmpl_len > 0) {
 			wr_len = write(STDOUT_FILENO, buf + cmpl_len, rd_len - cmpl_len);
 			if (wr_len == -1) {
 				write(STDERR_FILENO, "WRITE ERROR\n", 13);
-				break;
+				continue;
 			}
 			cmpl_len += wr_len;
 		}
